f1b214ab50d1fd42dc5047295cf02fcf3c796699
spicyzboss <bossthth1209mine@gmail.com>
2022-11-22 12:45:27 +0700

    finish

---
+++ auth-service/src/main/java/com/example/authservice/services/JwtUtil.java
@@ -0,0 +1,76 @@
+package com.example.authservice.services;
+
+import com.example.authservice.entities.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private String expirationTime;
+
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public Claims getAllClaimsFromToken(String token) {
+        System.out.println("Token: " + token);
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getAllClaimsFromToken(token).getExpiration();
+    }
+
+    public boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    public String generate(User userV0, String type) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", userV0.getId());
+        claims.put("role", userV0.getRole());
+        return doGenerateToken(claims, userV0.getEmail(), type);
+    }
+
+    public String doGenerateToken(Map<String, Object> claims, String username, String type) {
+        long expirationTimeLong;
+        if ("ACCESS".equals(type)) {
+            expirationTimeLong = Long.parseLong(expirationTime) * 1000;
+        } else {
+            expirationTimeLong = Long.parseLong(expirationTime) * 1000 * 5;
+        }
+
+        final Date createdDate = new Date();
+        final Date expirationDate = new Date(createdDate.getTime() + expirationTimeLong);
+
+        return Jwts.builder()
+            .setClaims(claims)
+            .setSubject(username)
+            .setIssuedAt(createdDate)
+            .setExpiration(expirationDate)
+            .signWith(key)
+            .compact();
+    }
+
+    public boolean validateToken(String token) {
+        return !isTokenExpired(token);
+    }
+}
